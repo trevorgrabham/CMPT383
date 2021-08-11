4b9b8f8e62228989c6f1feee9cd9a2c03ce960ef
tgrabham <tgrabham@sfu.ca>
2021-08-10 19:13:46 -0700

    heroku app working?

--- FinalProject/finalproject/__init__.py
+++ FinalProject/finalproject/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finalProject.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mhxropawgyehbz:21e76b4d3cb65882b0be14ba08506421d8911badac2549b406854d5d639a559b@ec2-3-233-100-43.compute-1.amazonaws.com:5432/d3ank5j78iqd1q'
 
 
 db = SQLAlchemy(app)
